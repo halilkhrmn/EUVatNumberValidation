a6f3c37cd7b082958ce1d61c36b7c444199dc7b2
Halil Kahraman <52932792+hibrhmk@users.noreply.github.com>
2021-09-16 16:09:21 +0300

    spaghetti to Class

--- index.php
+++ index.php
@@ -1,18 +1,71 @@
 <?php
-//Samples
-$vatnumberInput = "57753302306";
-$countryISO2 = "FR";
+
+/**
+ * User: hibrhmk
+ * Date: 16.09.2021
+ */
 
 
-//Code begin
-$country = $countryISO2;
-$vatnumber = $vatnumberInput;
-$url = 'http://ec.europa.eu/taxation_customs/vies/viesquer.do?ms=' . $country . '&iso=' . $country . '&vat=' . $vatnumber . '&name=&companyType=&street1=&postcode=&city=&BtnSubmitVat=Verify';
-$response = file_get_contents($url);
-preg_match('/<span class="validStyle">([^<]+)<\/span>/', $response, $matches);
-$status = false;
-if(isset($matches[0])){
-    if (strpos($matches[0], 'Yes') !== false) $status = true;
+class EUVatNumberValidation
+{
+    public $debugMode = false;
+
+    /**
+     * EUVatNumberValidation constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * check vat number
+     *
+     * @param $postParams
+     * @param string $action
+     * @return array
+     */
+    public function check($vatNumber = null, $countryISO2 = null)
+    {
+        try {
+            if (!$vatNumber || !$countryISO2) return array(
+                'success' => false,
+                'msg' => 'Information is missing'
+            );
+            $result = false;
+            $url = 'http://ec.europa.eu/taxation_customs/vies/viesquer.do?ms=' . $countryISO2 . '&iso=' . $countryISO2 . '&vat=' . $vatNumber . '&name=&companyType=&street1=&postcode=&city=&BtnSubmitVat=Verify';
+            $response = file_get_contents($url);
+            preg_match('/<span class="validStyle">([^<]+)<\/span>/', $response, $matches);
+            if (isset($matches[0])) {
+                if (strpos($matches[0], 'Yes') !== false) $result = true;
+            }
+
+            if ($this->debugMode) {
+                return array(
+                    'success' => true,
+                    'vatNumber' => $vatNumber,
+                    'countryISO2' => $countryISO2
+                );
+            } else {
+                return array(
+                    'success' => true,
+                    'vatNumber' => $vatNumber,
+                    'countryISO2' => $countryISO2
+                );
+            }
+        } catch (\Throwable $th) {
+            if ($this->debugMode) {
+                return array(
+                    'success' => false,
+                    'msg' => $th->getMessage(),
+                    'file' => $th->getFile(),
+                    'line' => $th->getLine()
+                );
+            } else {
+                return array(
+                    'success' => false,
+                    'msg' => 'An error occurred please try again'
+                );
+            }
+        }
+    }
 }
-
-print_r(array('rsp' => $status,'link' => $url));
